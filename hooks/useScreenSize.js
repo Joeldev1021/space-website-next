ec5833bf0cb4044652da7a66e038e9ffde9ffb1c
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-23 08:42:18 -0500

    hooks useScreenSize

--- hooks/useScreenSize.js
+++ hooks/useScreenSize.js
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(0);
 
   useEffect(() => {
+    if(window){
     window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("resize", handleResize);
-    };
+    }
+  }
   }, []);
+  useEffect(() => {
+     setWidth(window.innerWidth);
+  }, []);
+  
+
 
   const handleResize = () => {
     setWidth(window.innerWidth);
